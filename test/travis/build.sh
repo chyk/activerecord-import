be4215f479ed94c1de61a1922f0823452c1f70c6
OCHIAI Yuuki <ochiai@mediba.jp>
2016-01-26 10:48:29 +0900

    wip

--- test/travis/build.sh
+++ test/travis/build.sh
@@ -7,17 +7,11 @@ function run {
   $@
 }
 
-for activerecord_version in "3.1" "3.2" "4.1" "4.2" ; do
+for activerecord_version in "4.2" ; do
   export AR_VERSION=$activerecord_version
 
   bundle update activerecord
 
-  run run bundle exec rake test:em_mysql2               # Run tests for em_mysql2
-  run bundle exec rake test:mysql                   # Run tests for mysql
-  run bundle exec rake test:mysql2                  # Run tests for mysql2
-  run bundle exec rake test:mysql2spatial           # Run tests for mysql2spatial
-  run bundle exec rake test:mysqlspatial            # Run tests for mysqlspatial
-  run bundle exec rake test:postgis                 # Run tests for postgis
   run bundle exec rake test:postgresql              # Run tests for postgresql
   run bundle exec rake test:seamless_database_pool  # Run tests for seamless_database_pool
   run bundle exec rake test:spatialite              # Run tests for spatialite
